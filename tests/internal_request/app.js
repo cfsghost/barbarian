e54a2c563058e87f0b9d4ba8aeab1ee19125aabe
Fred Chien <fred@mandice.com>
2013-05-22 22:35:02 +0800

    add a new testcase to test loading resources

--- tests/internal_request/app.js
+++ tests/internal_request/app.js
@@ -5,9 +5,7 @@ var Barbarian = require('../../');
 var barbarian = new Barbarian();
 
 barbarian.init(function() {
-	barbarian.createWindow('barbarian://content/');
-	//barbarian.createWindow('barbarian://content/1aa/23.html');
-	//barbarian.createWindow('barbarian://engine');
+	barbarian.createWindow('barbarian://engine');
 });
 
 barbarian.on('request', function(req, res) {
